39e7731e73d6b5cdf8958decacbae177aa6e4a6d
unknown <vinayakotwal8055@gmail.com>
2025-11-01 00:47:13 +0530

    Setup Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base : '/AssetHub/',
+  build : {
+    outDir : 'docs'
+  },
   plugins: [
     react(),
     tailwindcss(),
